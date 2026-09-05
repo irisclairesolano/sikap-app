6a8bd82afbcccebc9aadc9863b37b5b4d16e343a
iristhefflower <iris.solano@sorsu.edu.ph>
2026-09-05 17:44:03 +0800

    feat(mobile): add SkeletonCard placeholder component

---
+++ src/components/common/SkeletonCard.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { JobCardSkeleton, DashboardSkeleton, Skeleton } from './SkeletonLoader';
+
+export const SkeletonCard = JobCardSkeleton;
+export { JobCardSkeleton, DashboardSkeleton, Skeleton };
+export default SkeletonCard;
